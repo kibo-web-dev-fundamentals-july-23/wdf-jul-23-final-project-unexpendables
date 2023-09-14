62f2c667bc6763fddc8fd6c19152e8b4d8ecd6bd
Glory <glory.emmanuel@kibo.school>
2023-09-14 01:25:39 +0100

    Made changes

--- final-project-the-unexpendables-main/script.js
+++ final-project-the-unexpendables-main/script.js
@@ -1,23 +1,24 @@
-// JavaScript for the slideshow
-var slideIndex = 1;
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all the FAQ question and answer elements
+  const faqItems = document.querySelectorAll('.faq-item');
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
+  // Add click event listeners to each question
+  faqItems.forEach(faqItem => {
+    const question = faqItem.querySelector('h3'); // Updated selector to target the questions
+    const answer = faqItem.querySelector('.faq-answer');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    question.addEventListener('click', () => {
+      // Toggle the 'active' class to show/hide the answer
+      answer.classList.toggle('active');
+    });
 
-showSlides(slideIndex);
+    // Add hover event listeners to each question
+    question.addEventListener('mouseenter', () => {
+      answer.classList.add('active');
+    });
+
+    question.addEventListener('mouseleave', () => {
+      answer.classList.remove('active');
+    });
+  });
+});
